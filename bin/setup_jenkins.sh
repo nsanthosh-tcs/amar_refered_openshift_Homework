fc06ed1251f3e5b939ecb1168921b14f3b86fe35
amar134 <41107381+amar134@users.noreply.github.com>
2019-07-04 14:28:18 +0530

    Update setup_jenkins.sh

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+##!/bin/bash
 # Setup Jenkins Project
 if [ "$#" -ne 3 ]; then
     echo "Usage:"
@@ -11,33 +11,65 @@ GUID=$1
 REPO=$2
 CLUSTER=$3
 
+# export GUID=2c88							                          # noch an Homework GUID anpassen!
+# export REPO=https://github.com/DevOpsDirk/homework			# noch an repo anpassen!
+# export CLUSTER=na311.openshift.opentlc.com				      # noch an Homework Cluster anpassen!
+
+
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
-oc new-app jenkins-ephemeral --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
+# Set up Jenkins with sufficient resources
+# oc new-app --template jenkins-persistent \
+#   -n $GUID-jenkins \
+#   -p ENABLE_OAUTH=false \
+#   -p MEMORY_LIMIT=2Gi \
+#   -p VOLUME_CAPACITY=4Gi \
+#   -p DISABLE_ADMINISTRATIVE_MONITORS=true \
+
+# ohne persistent storage due to missing shared storage on cluster
+oc new-app --template jenkins-ephemeral \
+   -n $GUID-jenkins \
+   -p ENABLE_OAUTH=true \
+   -p MEMORY_LIMIT=2Gi \
+   -p DISABLE_ADMINISTRATIVE_MONITORS=true \
+
+
+
 # Create custom agent container image with skopeo
-# Jenkins Slave Image:
-oc new-build  -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
-      USER root\nRUN yum -y install skopeo && yum clean all\n
-      USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
+oc -n $GUID-jenkins new-build \
+			-D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
+      			USER root\nRUN yum -y install skopeo && yum clean all\n
+      			USER 1001' \
+			--name=jenkins-agent-appdev \
+			-n ${GUID}-jenkins
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
-# Jenkins Pipeline:
-echo "apiVersion: v1
-items:
-- kind: "BuildConfig"
-  apiVersion: "v1"
-  metadata:
-    name: "tasks-pipeline"
-  spec:
-    source:
-      type: "Git"
-      git:
-        uri: "https://github.com/rkuntze/advdev_homework.git"
-    strategy:
-      type: "JenkinsPipeline"
-      jenkinsPipelineStrategy:
-        jenkinsfilePath: openshift-tasks/Jenkinsfile
-kind: List
-metadata: []" | oc create -f - -n ${GUID}-jenkins
+oc -n $GUID-jenkins new-build ${REPO} \
+   --strategy pipeline \
+   --env GUID=${GUID} \
+   --env CLUSTER=${CLUSTER} \
+   --env NEXUS_RELEASE_URL='http://nexus3.gpte-hw-cicd.svc:8081/repository/releases' \
+   --env REPO=${REPO} \
+   --context-dir openshift-tasks \
+   --name=tasks-pipeline
+
+# why doesn`t the above not work? where is "name: tasks-pipeline" to be set? regards - Dirk
+#echo "apiVersion: v1
+#items:
+#- kind: "BuildConfig"
+#  apiVersion: "v1"
+#  metadata:
+#    name: "tasks-pipeline"
+#  spec:
+#    source:
+#      type: "Git"
+#      git:
+#        uri: "${REPO}"
+#    strategy:
+#      type: "JenkinsPipeline"
+#      jenkinsPipelineStrategy:
+#        jenkinsfilePath: Jenkinsfile
+#kind: List
+#metadata: []" | oc create -f - -n ${GUID}-jenkins
 
 
 # Make sure that Jenkins is fully up and running before proceeding!
